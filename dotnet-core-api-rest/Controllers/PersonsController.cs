2df55a587eaa0e8a3bd2575e47434a6055a27cbc
Anderson Rodrigues <andersoonluan@gmail.com>
2018-09-22 20:17:45 -0300

    implementation person class

--- dotnet-core-api-rest/Controllers/PersonsController.cs
+++ dotnet-core-api-rest/Controllers/PersonsController.cs
@@ -8,7 +8,7 @@ namespace dotnet_core_api_rest.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class PersonsController : ControllerBase
     {
         // GET api/values
         [HttpGet]
---
+++ dotnet-core-api-rest/Model/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace dotnetcoreapirest.Model
+{
+    public class Person
+    {
+		public long Id { get; set; }
+		public string Name { get; set; }
+		public string LastName { get; set; }
+		public string Country { get; set; }
+		public string City { get; set; }
+		public string Address { get; set; }
+		public string Gender { get; set; }
+		public int Age { get; set; }
+		public DateTime Birthday { get; set; }
+		public string Email { get; set; }
+
+    }
+}
